8cbe92d5339287c6a408081099c76f64d9c7cd77
Oscar Dalby <129061939+OscarDalby@users.noreply.github.com>
2024-07-26 00:09:53 +0100

    minor refactors

--- img_to_pix.go
+++ img_to_pix.go
@@ -12,23 +12,24 @@ import (
 type ProcessConfig struct {
 	pixel_size int
 	scaling    int
+	bg_color   color.RGBA
 }
 
+var config = ProcessConfig{pixel_size: 32, scaling: 1, bg_color: color.RGBA{0, 0, 0, 0}}
+
 func main() {
-	base_img, err := get_base_image("./input", "selfie")
+	base_img, err := get_base_image("./input", "selfie", config)
 	if err != nil {
 		log.Fatalf("Failed to get base image: %v", err)
 	}
-	config := ProcessConfig{pixel_size: 64, scaling: 1}
 	processed_img, err := process_png(base_img, config)
 	if err != nil {
 		log.Fatalf("Failed to process PNG: %v", err)
 	}
-
 	create_png(processed_img, "./output", "output_image")
 }
 
-func get_base_image(base_path string, file_name string) (image.Image, error) {
+func get_base_image(base_path string, file_name string, config ProcessConfig) (image.Image, error) {
 	var path = fmt.Sprintf("%s/%s.png", base_path, file_name)
 	file, err := os.Open(path)
 	if err != nil {
@@ -36,34 +37,17 @@ func get_base_image(base_path string, file_name string) (image.Image, error) {
 		return nil, err
 	}
 	defer file.Close()
-
 	base_img, err := png.Decode(file)
+	if base_img.Bounds().Dx()%config.pixel_size != 0 {
+		panic("pixel_size must divide width")
+	}
+	if base_img.Bounds().Dy()%config.pixel_size != 0 {
+		panic("pixel_size must divide height")
+	}
 	if err != nil {
 		fmt.Println("Error decoding file:", err)
 		return nil, err
 	}
-
-	bounds := base_img.Bounds()
-	var imageWidth = bounds.Dx()
-	var imageHeight = bounds.Dy()
-	fmt.Println("Width:", imageWidth, "Height:", imageHeight)
-	var maxPixels int = 64
-
-	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-		if y >= maxPixels {
-			break
-		}
-		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			if x >= maxPixels {
-				break
-			}
-
-			pixel := base_img.At(x, y)
-			r, g, b, a := pixel.RGBA()
-			fmt.Printf("Pixel at (%d,%d) - R:%d, G:%d, B:%d, A:%d\n", x, y, r>>8, g>>8, b>>8, a>>8)
-		}
-	}
-
 	return base_img, nil
 }
 
@@ -93,13 +77,23 @@ func process_png(base_img image.Image, config ProcessConfig) (image.Image, error
 	fmt.Printf("%v", config)
 	var width = base_img.Bounds().Dx()
 	var height = base_img.Bounds().Dy()
+
+	if width%config.pixel_size != 0 {
+		panic("pixel_size must divide width")
+	}
+	if height%config.pixel_size != 0 {
+		panic("pixel_size must divide height")
+	}
+
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 
-	// for y := 0; y < height; y++ {
-	// 	for x := 0; x < width; x++ {
-	// 		img.Set(x, y, color.RGBA{255, 255, 255, 255})
-	// 	}
-	// }
+	if config.bg_color.A != 0 { // if the bg_color is not completely transparent, then fill the background before the remaining processing
+		for y := 0; y < height; y++ {
+			for x := 0; x < width; x++ {
+				img.Set(x, y, config.bg_color)
+			}
+		}
+	}
 
 	for y := 0; y < height; y += config.pixel_size {
 		for x := 0; x < width; x += config.pixel_size {
@@ -107,18 +101,11 @@ func process_png(base_img image.Image, config ProcessConfig) (image.Image, error
 			var avg_color color.RGBA
 			count := 0
 			for dy := 0; dy < config.pixel_size; dy++ {
-				// if dy >= height {
-				// 	break
-				// }
 				for dx := 0; dx < config.pixel_size; dx++ {
-					// if dx >= width {
-					// 	break
-					// }
 					pixel := base_img.At(x+dx, y+dy)
 					r, g, b, a := pixel.RGBA()
 					color_list = append(color_list, color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)})
 					avg_color = average_color(color_list)
-					// iterate on x from x -> x + config.pixel_size
 					for j := 0; j < config.pixel_size; j++ {
 						for i := 0; i < config.pixel_size; i++ {
 							img.Set(x+i, y+j, avg_color)

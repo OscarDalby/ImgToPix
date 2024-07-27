2dd52c8f6fc4dd56f284989301e341f6e0777eda
Oscar Dalby <129061939+OscarDalby@users.noreply.github.com>
2024-07-27 15:45:19 +0100

    refactored to a full process pipeline

--- img_to_pix.go
+++ img_to_pix.go
@@ -15,18 +15,35 @@ type ProcessConfig struct {
 	bg_color   color.RGBA
 }
 
+var input_path = "./input"
+var input_filename = "input"
+var tmp_path = "./tmp"
+var tmp_filename = "working"
+var output_path = "./output"
+var output_filename = "output"
+
 var config = ProcessConfig{pixel_size: 32, scaling: 3, bg_color: color.RGBA{0, 0, 0, 0}}
 
 func main() {
-	base_img, err := get_base_image("./input", "selfie", config)
+	fmt.Printf("image processing started, working image stored in %s/%s\n", tmp_path, tmp_filename)
+	var working_image image.Image
+	var err error
+	base_img, err := get_base_image(input_path, input_filename, config)
 	if err != nil {
 		log.Fatalf("Failed to get base image: %v", err)
 	}
-	processed_img, err := process_png(base_img, config)
+	working_image, err = process_png_pixelise(base_img, config)
 	if err != nil {
 		log.Fatalf("Failed to process PNG: %v", err)
 	}
-	create_png(processed_img, "./output", "output_image")
+
+	create_png(working_image, output_path, "pixelated")
+
+	working_image, err = process_png_apply_palette(working_image, config)
+	if err != nil {
+		log.Fatalf("Failed to process PNG: %v", err)
+	}
+	create_png(working_image, output_path, output_filename)
 }
 
 func get_base_image(base_path string, file_name string, config ProcessConfig) (image.Image, error) {
@@ -73,16 +90,11 @@ func average_color(colors []color.RGBA) color.RGBA {
 	return color.RGBA{uint8(average_r), uint8(average_g), uint8(average_b), uint8(average_a)}
 }
 
-func process_png(base_img image.Image, config ProcessConfig) (image.Image, error) {
+func process_png_pixelise(base_img image.Image, config ProcessConfig) (image.Image, error) {
 	var width = base_img.Bounds().Dx()
 	var height = base_img.Bounds().Dy()
 	var scaled_width = width * config.scaling / config.pixel_size
 	var scaled_height = height * config.scaling / config.pixel_size
-
-	if width%config.pixel_size != 0 || height%config.pixel_size != 0 {
-		panic("pixel_size must evenly divide both width and height")
-	}
-
 	scaled_img := image.NewRGBA(image.Rect(0, 0, scaled_width, scaled_height))
 
 	for y := 0; y < height; y += config.pixel_size {
@@ -113,6 +125,42 @@ func process_png(base_img image.Image, config ProcessConfig) (image.Image, error
 		}
 	}
 	return scaled_img, nil
+}
+
+func process_png_apply_palette(base_img image.Image, config ProcessConfig) (image.Image, error) {
+	var width = base_img.Bounds().Dx()
+	var height = base_img.Bounds().Dy()
+	fmt.Printf("%v", config)
+	colored_img := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	// for y := 0; y < height; y += config.pixel_size {
+	// 	for x := 0; x < width; x += config.pixel_size {
+	// 		var totalR, totalG, totalB, totalA, count uint32
+	// 		var colors_in_block []color.RGBA
+	// 		for dy := 0; dy < config.pixel_size; dy++ {
+	// 			for dx := 0; dx < config.pixel_size; dx++ {
+	// 				pixel := base_img.At(x+dx, y+dy)
+	// 				r, g, b, a := pixel.RGBA()
+	// 				colors_in_block = append(colors_in_block, color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)})
+	// 				totalR += r
+	// 				totalG += g
+	// 				totalB += b
+	// 				totalA += a
+	// 				count++
+	// 			}
+	// 		}
+	// 		avg_color := average_color(colors_in_block)
+
+	// 		for sy := 0; sy < config.scaling; sy++ {
+	// 			for sx := 0; sx < config.scaling; sx++ {
+	// 				scaled_x := (x/config.pixel_size)*config.scaling + sx
+	// 				scaled_y := (y/config.pixel_size)*config.scaling + sy
+	// 				scaled_img.Set(scaled_x, scaled_y, avg_color)
+	// 			}
+	// 		}
+	// 	}
+	// }
+	return colored_img, nil
 }
 
 func create_png(img image.Image, base_path string, file_name string) {
